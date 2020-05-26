8da1e300ecbdacc6a3699baf487256a1d7221af2
Kenyu Kobayashi <kenyukobayashi@hotmail.com>
2020-05-26 15:19:09 +0200

    draft country menu

--- HTML/stats/stats.js
+++ HTML/stats/stats.js
@@ -49,6 +49,37 @@ whenDocumentLoaded(() => {
 	criterion_menu.innerHTML += criterion_HTML;
 	*/
 
+	// Load the country search
+	let country_data = [];
+	let country_names = new Set([]);
+	for (c in country_distributions){
+		// Parse elements
+		const els = c.substring(1, c.length - 1).split(",");
+		const question_id = els[0].substring(1, els[0].length - 1);
+		const country_name = els[1].substring(2, els[1].length - 1);
+		const country_code = els[2].substring(2, els[2].length - 1);
+		// Get Distribution
+		let distribution = [];
+		for (n in ["0.0", "1.0", "2.0", "3.0", "4.0", "5.0"]){
+			distribution.push(country_distributions[c][n.toString()+".0"]);
+		}
+		country_names = country_names.add(country_name);
+		country_data.push({"id": question_id, "country": country_name, "country_code": country_code,
+		"distribution": distribution, "x": ["0", "1", "2", "3", "4", "5"]});
+	}
+	// Load the search dropdown menu and fill it
+	let country_menu = document.getElementById("country_list");
+	let country_menu_HTML = "";
+	for (let c_name of country_names){
+		console.log(c_name);
+		country_menu_HTML += "<li>"+c_name+"</li>";
+	}
+	country_menu.innerHTML += country_menu_HTML;
+
+/*	for (k in question_corpus){
+
+	}*/
+
 	// Load data in the bar plot
 	let data = [];
 	for (k in questions_corpus){
